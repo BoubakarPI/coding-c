1a81e7e535894494f60fb97707ae0160f9c97734
Boubakar PI <agenceweb1919@gmail.com>
2024-04-11 23:07:37 +0000

    11 avr. 2024, 23:07

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, z, x, y;
+    x = 2; 
+    y = 10;
+    z = 1;
+    i = 1;
+    
+    while (i <= y) {
+        z = z * x;
+        i++;
+    }
+    
+    printf("\nDeux à la puissance dix font %d\n", z);
+    
+    return 0;
+}
