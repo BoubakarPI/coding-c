fb46ba3d21a617d7980f5160a882334c790b08a3
Boubakar PI <agenceweb1919@gmail.com>
2024-04-12 10:22:49 +0000

    12 avr. 2024, 10:22

--- proh.c
+++ proh.c
@@ -3,7 +3,7 @@ main()
 {
 int i, k, t, Position1 = 50, Position2 = 100, Position3 = 7;
 i = -10;
-t = Position1 * Position2; 
+t = Position1 * Position2; /* t = 5000*/
 k = t * Position3; 
 printf("%d\n", k);
 
@@ -12,6 +12,4 @@ float x = 15.4;
 printf("Le n %3d",n);
 printf("Le x %10f",x);
 /* Ce nombre précise le nombre de positions sur lequel doit s'écrire la valeur de la variable, cadrée
- */
-
-}
+ */}
