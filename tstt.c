308624263977dc5f3cd25f238b6f54926b68c3f5
Boubakar PI <agenceweb1919@gmail.com>
2024-04-12 10:46:11 +0000

    12 avr. 2024, 10:46

---
+++ tstt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    float HT, TTC;
+     printf("Saisir le prix HT : ");
+     scanf("%f", &HT);
+
+      if (HT < 150000)
+        TTC = HT *1.2;
+      else 
+        TTC = HT*1.02;
+
+      printf("Le TTC est de %f", TTC);
+
+}
