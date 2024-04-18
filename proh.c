a3eff414bbbe423cf5ea6e1a4050a4a8b9a6ebb0
Boubakar PI <agenceweb1919@gmail.com>
2024-04-18 22:59:15 +0000

    18 avr. 2024, 22:59

--- proh.c
+++ proh.c
@@ -3,7 +3,7 @@
 int main() {
     int i = 0;
     char count = 'a';
-    printf("Entrez un nombre que vous voulez");
+    printf("Entrez un nhhombre que vous voulez");
 
     while(count != 0) {
         printf("Vous êteos au %d essai", i);
