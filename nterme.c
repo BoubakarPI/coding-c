c70da1d629374b3a7654fb1b881f1438531872f4
Boubakar PI <agenceweb1919@gmail.com>
2024-04-23 10:45:10 +0000

    23 avr. 2024, 10:45

--- nterme.c
+++ nterme.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
-float main() {
-    
+float terme(int n) {
+
+    if (n==0)
+        return 2;
+    if (n==1)
+        return 3;
+    else
+        return (2/3)*terme(n-1)-(1/4)*terme(n);
+
+}
+
+int main() {
+int n;
+
+printf("Entrez n");
+scanf("%d", &n); 
+
+terme(n);
+
 }
