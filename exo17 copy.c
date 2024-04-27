05775765a2ee4e0018bed4aee5dc6f5293fa1f22
Boubakar PI <agenceweb1919@gmail.com>
2024-04-27 09:32:42 +0000

    27 avr. 2024, 09:32

--- exo17 copy.c
+++ exo17 copy.c
@@ -17,6 +17,6 @@ int main() {
     int t[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     insert(t, 10, 11);
     for (int i = 0; i < 11; i++) {
-        printf("%d ", t[i]);
+        printf("%d\t", t[i]);
     }
 }
