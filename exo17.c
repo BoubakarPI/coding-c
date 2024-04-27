10da6ace8d309bccb8c35787a9021c0a5afa9475
Boubakar PI <agenceweb1919@gmail.com>
2024-04-27 09:04:51 +0000

    27 avr. 2024, 09:04

---
+++ exo17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void insert(int t[], int n, float x) {
+    int i = 1;
+
+    while (x < t[n-i+1]) {
+        t[n-i+2] = t[n-i+1];
+        i++;
+    }
+    t[n-i+2] = x;
+}
+int main() {
+    int t[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    insert(t, 10, 11);
+    for (int i = 0; i < 11; i++) {
+        printf("%d ", t[i]);
+    }
+}
