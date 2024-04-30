349674c47b5c645847593ecb092fc1525fae21a5
Boubakar PI <agenceweb1919@gmail.com>
2024-04-30 16:04:03 +0000

    30 avr. 2024, 16:04

--- Pointeur/saisi.c
+++ Pointeur/saisi.c
@@ -2,4 +2,18 @@
 
 int main(void) {
     int Tab[5][4];
+
+    int *P[5];
+    int i, j;
+
+    for (i=0; i<5; i++) {
+        P[i] = Tab[i];
+    }
+
+    for (i = 0; i <5; i++) {
+        for (j=0; j<4; j++) {
+            *(*(P+i) + j) = 1;
+        }
+    }
+    return 0;
 }
