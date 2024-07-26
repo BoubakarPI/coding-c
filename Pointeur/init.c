85aee4cb2d9f61df163b41d3e221efae2c3f4656
Boubakar PI <boubakarpi@proton.me>
2024-07-26 01:47:18 +0000

    MàJ

--- Pointeur/init.c
+++ Pointeur/init.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     int x, y, s;
     int *p;
 
     printf("Entrez le premier nbre 1 et le nbre 2 : ");
-    scanf("%d %d", x, y);
+    scanf("%d %d", &x, &y);
 
     s = x + y;
 
     p = &s;
 
-    printf("La somme de %d et %d  donne : %d", x, y, p);
-}    
+    printf("La somme de %d et %d  donne : %d", x, y, *p);
+}
